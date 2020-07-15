3dbf2c04b217e5a2fcc76d38b66545f57177c03d
Hatam <68222201+hatamabolghasemi@users.noreply.github.com>
2020-07-15 19:09:35 +0430

    Add files via upload

--- SERVER.py
+++ SERVER.py
@@ -1,2 +1,19 @@
 import socket
 import threading
+
+class SERVER:
+
+
+    def create_server():                    # to be able to create server at every system
+
+
+    def receive_messages():                 # to indicate receiving settings
+
+    
+    def broadcast_all():                    # to broadcast the receive buffer size info if the client is not sender
+
+
+    def receive_message_new_thread():       # to make a thread for a client if it's accepted
+
+
+    def add_to_clients():                   # to add a client to the client list
