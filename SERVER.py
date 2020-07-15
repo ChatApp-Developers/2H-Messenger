1b3c0b164cabe29a67e622bbe06fb333a68a1486
Hatam <68222201+hatamabolghasemi@users.noreply.github.com>
2020-07-15 19:52:33 +0430

    Add files via upload

--- SERVER.py
+++ SERVER.py
@@ -2,9 +2,21 @@ import socket
 import threading
 
 class SERVER:
+    client_list = []
+    last_received_message = ""
+
+
+    def __init__(self):                     # to initialize server socket as none and create it
+        self.server_socket = None
+        self.create_server()
 
 
     def create_server():                    # to be able to create server at every system
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        local_ip = '127.0.0.1'
+        local_port = 10319
+        self.server_socket.bind((local_ip, local_port))
+        self.server_socket,listen()
 
 
     def receive_messages():                 # to indicate receiving settings
